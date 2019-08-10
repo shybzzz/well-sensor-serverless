6876050c21728a66da380f0c9d844192dda23fc4
shybzzz <rostykrom@yahoo.com>
2019-08-10 07:18:30 +0300

    count device records per period is introduced

--- mqtt-data-service/data-repository.ts
+++ mqtt-data-service/data-repository.ts
@@ -1,14 +1,54 @@
 import { client } from './athena-manager';
 import { AthenaExecutionResult } from 'athena-client';
+import { DataRecord } from './data-record';
+
+export function getLastRecords(
+  device: string,
+  limit: string
+): Promise<AthenaExecutionResult<DataRecord>> {
+  return client
+    .execute<DataRecord>(
+      `
+      SELECT * 
+      FROM combined_sensor.data 
+      WHERE device=\'${device}\' 
+      ORDER BY date DESC LIMIT ${limit};
+      `
+    )
+    .toPromise();
+}
 
 export function getLastSeen(
   device: string
 ): Promise<AthenaExecutionResult<{ date: string }>> {
   return client
-    .execute<{ date }>(
-      `SELECT date FROM combined_sensor.data WHERE device=\'${device}\' ORDER BY date DESC limit 1;`
+    .execute<DataRecord>(
+      `
+      SELECT date FROM combined_sensor.data 
+      WHERE device=\'${device}\' 
+      ORDER BY date DESC LIMIT 1;
+      `
     )
     .toPromise();
 }
 
-export const DataRepository = { getLastSeen };
+export function countPerPeriod(
+  device: string,
+  dateTrunc: string,
+  startDate: string,
+  endDate: string
+): Promise<AthenaExecutionResult<{ count: string }>> {
+  return client
+    .execute<{ count: string }>(
+      `
+      SELECT ${dateTrunc}, COUNT(*) AS count
+      FROM  "combined_sensor"."${device}"
+      WHERE ${dateTrunc} >= CAST('${startDate}' AS DATE)
+      AND ${dateTrunc} < CAST('${endDate}' AS DATE)
+      GROUP BY ${dateTrunc}
+      `
+    )
+    .toPromise();
+}
+
+export const DataRepository = { getLastRecords, getLastSeen, countPerPeriod };
--- mqtt-data-service/handler.ts
+++ mqtt-data-service/handler.ts
@@ -14,3 +14,51 @@ export const getLastSeen: APIGatewayProxyHandler = async (event, context) => {
     return failure('Could not find last record');
   }
 };
+
+export const getLastRecords: APIGatewayProxyHandler = async (
+  event,
+  context
+) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    const queryStringParameters = event.queryStringParameters;
+    const limit = queryStringParameters && queryStringParameters.limit;
+    const device = queryStringParameters && queryStringParameters.device;
+    if (!limit) {
+      return failure('No Data Limit');
+    }
+    const lastSeen = await DataRepository.getLastRecords(device, limit);
+    return success(lastSeen.records);
+  } catch (er) {
+    return failure('Could not find last records');
+  }
+};
+
+export const countPerPeriod: APIGatewayProxyHandler = async (
+  event,
+  context
+) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    const pathParameters = event.pathParameters;
+    const dateTrunc = pathParameters && pathParameters.dateTrunc;
+
+    const queryStringParameters = event.queryStringParameters;
+    const startDate = queryStringParameters && queryStringParameters.startDate;
+    const endDate = queryStringParameters && queryStringParameters.endDate;
+    const device = queryStringParameters && queryStringParameters.device;
+
+    if (!(dateTrunc && startDate && endDate && device)) {
+      return failure('Missing parameters');
+    }
+    const countPerPeriod = await DataRepository.countPerPeriod(
+      device,
+      dateTrunc,
+      startDate,
+      endDate
+    );
+    return success(countPerPeriod.records);
+  } catch (er) {
+    return failure(`Could not count per period`);
+  }
+};
